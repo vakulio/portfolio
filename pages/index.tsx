141befd9bc92c9237879ba9d374df7f00702e4a1
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2022-12-03 01:25:45 +0300

    try

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ type Props = {
 const Home = ({ pageInfo, experience, skills, projects, socials }: Props) => {
 	return (
 		<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-			<Head>
+			{/* <Head>
 				<title>{pageInfo?.name} Portfolio</title>
 			</Head>
 			<Header socials={socials} />
@@ -57,7 +57,7 @@ const Home = ({ pageInfo, experience, skills, projects, socials }: Props) => {
 						<Image width={50} height={50} className="w-10 h-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" src={urlFor(pageInfo.personImage).url()} alt="personPhoto" />
 					</div>
 				</footer>
-			</Link>
+			</Link> */}
 		</div>
 	)
 }
