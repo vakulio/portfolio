2031e99dfd5aed7bb1dc41fc2aca032e405e1c89
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2022-12-02 23:03:28 +0300

    env next

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL:'https://portfolio-alpha-steel-29.vercel.app/'
+  },
   images: {
     domains: ['cdn.sanity.io'],
     },
